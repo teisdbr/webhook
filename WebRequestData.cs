539d6eb2b227375b4ad5b53915cc16cab58f9253
Brian Truman <34318282+bttruman@users.noreply.github.com>
2021-11-09 12:34:41 -0600

    Add files via upload

---
+++ WebRequestData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebhookProcessor
+{
+    public class WebRequestData<T>
+    {
+        public string queryParams { get; set; }
+
+        public T payload { get; set; }
+
+        public WebRequestData(string queryParams, T payload)
+        {
+            this.queryParams = queryParams;
+            this.payload = payload;
+        }
+
+        public WebRequestData()
+        {
+
+        }
+    }
+}
+
